d5148615327799af5d897154947350e654ae28d1
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2020-05-05 23:58:39 -0400

    Refactored Dummy Seeder for the model. Added tests to check correct generation.

--- tests/Model/SeedersTest.php
+++ tests/Model/SeedersTest.php
@@ -64,6 +64,117 @@ class SeedersTest extends TestCase
         $this->assertFileNotExistsInFilesystem($this->app->databasePath('seeds' . DS . 'UserSeeder.php'));
     }
 
+    public function test_replaces_model_strings_in_seeder()
+    {
+        $this->mockDatabaseFile([
+            'models' => [
+                'User' => [
+                    'name' => 'string',
+                ],
+            ],
+        ]);
+
+        $this->shouldMockSeederFile(false);
+
+        $this->artisan('larawiz:scaffold');
+
+        $content = $this->filesystem->get($this->app->databasePath('seeds' . DS . 'UserSeeder.php'));
+
+        $this->assertEquals(<<<'CONTENT'
+<?php
+
+use LogicException;
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factory;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @param  \Illuminate\Database\Eloquent\Factory  $factory
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function run(Factory $factory, User $user)
+    {
+        if ($this->modelHasFactory($factory)) {
+            $this->createRecords($factory, $this->amount($user));
+
+            $this->createAdditionalRecords($factory);
+
+            return $this->createStates($factory);
+        }
+
+        throw new LogicException('The [User] has no factory defined to use for seeding.');
+    }
+
+    /**
+     * Creates additional records to populate the database.
+     *
+     * @param  \Illuminate\Database\Eloquent\Factory  $factory
+     */
+    protected function additionalRecords($factory)
+    {
+        // This method is a convenient way to add personalized records.
+        //
+        // $factory->of(User::class)->create(['name' => 'John Doe']);
+    }
+
+    /**
+     * Creates additional record by using states.
+     *
+     * @param  \Illuminate\Database\Eloquent\Factory  $factory
+     * @return void
+     */
+    public function createStates($factory)
+    {
+        // If your User model has states defined, you can add them here too.
+        //
+        // $factory->of(User::class)->times(10)->state('state')->create();
+    }
+
+    /**
+     * Returns a useful amount of records to create.
+     *
+     * @param \App\User $user
+     * @return int
+     */
+    protected function amount($user)
+    {
+        // We will conveniently create to two and a half pages of User.
+        return (int) ($user->getPerPage() * 2.5);
+    }
+
+    /**
+     * Check if the model has a factory defined.
+     *
+     * @param  \Illuminate\Database\Eloquent\Factory  $factory
+     * @return bool
+     */
+    protected function modelHasFactory($factory)
+    {
+        return isset($factory[User::class]);
+    }
+
+    /**
+     * Populate the model records using the factory definition.
+     *
+     * @param  \Illuminate\Database\Eloquent\Factory  $factory
+     * @param  int  $amount
+     */
+    protected function createRecords($factory, int $amount)
+    {
+        $factory->of(User::class)->times($amount)->create();
+    }
+}
+
+CONTENT
+        ,$content);
+    }
+
     protected function tearDown() : void
     {
         $this->cleanProject();
