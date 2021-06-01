bae60e2ca1e1438e4c05a53a288a19070548e13a
DarkGhosthunter <DarkGhostHunter@gmail.com>
2021-06-01 19:03:10 -0400

    Updated tests for fixed Seeder.

--- tests/Model/SeedersTest.php
+++ tests/Model/SeedersTest.php
@@ -89,6 +89,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use LogicException;
 
 class UserSeeder extends Seeder
 {
